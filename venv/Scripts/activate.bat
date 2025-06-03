556732f6d88a2f9d280a1ea3ef0666a65f9e4bc1
Lucascgrs <lucascongras@hotmail.fr>
2025-06-03 15:31:45 +0200

    b

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lucas\OneDrive\Bureau\EPF\4A\Web Programming\WebPro_Project\library_app\venv
+set VIRTUAL_ENV=C:\Users\lucas\OneDrive\Bureau\EPF\4A\Web Programming\WebPro_Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
